bed3620219c45dd8ea6e9615d304d48d852d2fb9
Enzo Bustamante <enzobustamante895@gmail.com>
2024-10-16 16:11:54 -0300

    feat: finish turorial part

--- balloon.gd
+++ balloon.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
+@onready var hurtbox: Area2D = $Hurtbox
 
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
+func _ready():
+	hurtbox.body_entered.connect(_on_hurtbox_body_entered)
+
 func _physics_process(delta):
 	var input_vector = Input.get_vector("ui_left","ui_right", "ui_up", "ui_down")
 	velocity = input_vector * 100
 	move_and_slide()
+
+func _on_hurtbox_body_entered(body):
+	Events.balloon_popped.emit()
+	queue_free()
